63b7de7c0fadf665a10d6549299a170b69d9e42d
Eom Sanghyun <quarterMoon234@gmail.com>
2026-02-06 17:05:14 +0900

    feat: MemberPromoteService 비즈니스 로직 분리

--- member-service/src/main/java/com/thock/back/member/app/MemberPromoteService.java
+++ member-service/src/main/java/com/thock/back/member/app/MemberPromoteService.java
@@ -4,6 +4,7 @@ package com.thock.back.member.app;
 import com.thock.back.global.eventPublisher.EventPublisher;
 import com.thock.back.global.exception.CustomException;
 import com.thock.back.global.exception.ErrorCode;
+import com.thock.back.member.domain.command.PromoteToSellerCommand;
 import com.thock.back.member.domain.entity.Member;
 import com.thock.back.member.out.MemberRepository;
 import com.thock.back.shared.member.domain.MemberRole;
@@ -15,27 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class MemberUpdateService {
+public class MemberPromoteService {
 
     private final MemberRepository memberRepository;
     private final EventPublisher eventPublisher;
 
-    public void updateMemberRole(Long memberId, String bankCode, String accountNumber, String accountHolder) {
-        Member member = memberRepository.findById(memberId)
+    public void promoteToSeller(PromoteToSellerCommand command) {
+        Member member = memberRepository.findById(command.memberId())
                 .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
-        String memberRole = member.getRole().toString();
+        member.promoteToSeller(command.bankCode(), command.accountNumber(), command.accountHolder());
+        memberRepository.save(member);
 
-        if (member.getRole() == MemberRole.USER) {
-            member.setRole(MemberRole.SELLER);
-            member.setBankCode(bankCode);
-            member.setAccountNumber(accountNumber);
-            member.setAccountHolder(accountHolder);
-            memberRepository.save(member);
-
-            eventPublisher.publish(new MemberModifiedEvent(member.toDto()));
-        } else {
-            throw new CustomException(ErrorCode.INVALID_ROLE_PROMOTION);
-        }
+        eventPublisher.publish(new MemberModifiedEvent(member.toDto()));
     }
 }
