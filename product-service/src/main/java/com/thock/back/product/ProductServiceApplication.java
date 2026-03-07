a961018b7d5ee211ef65320b4969526f6122c5a4
Eom Sanghyun <quarterMoon234@gmail.com>
2026-03-07 23:14:01 +0900

    feat: INBOX_EVENT 테이블 스캔

--- product-service/src/main/java/com/thock/back/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/thock/back/product/ProductServiceApplication.java
@@ -2,13 +2,23 @@ package com.thock.back.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "com.thock.back.product",
         "com.thock.back.global"
 })
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {
+        "com.thock.back.product",
+        "com.thock.back.global.inbox.repository"
+})
+@EntityScan(basePackages = {
+        "com.thock.back.product",
+        "com.thock.back.global.inbox.entity"
+})
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
