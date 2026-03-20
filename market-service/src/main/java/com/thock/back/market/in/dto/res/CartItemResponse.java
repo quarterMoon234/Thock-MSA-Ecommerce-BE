6d63ce49713ee720b319bd8efd220b903cf77a20
Eom Sanghyun <quarterMoon234@gmail.com>
2026-03-20 10:58:56 +0900

    feat: 주문 모듈 장바구니 상품 정보 DTO 구현

--- market-service/src/main/java/com/thock/back/market/in/dto/res/CartItemResponse.java
+++ market-service/src/main/java/com/thock/back/market/in/dto/res/CartItemResponse.java
@@ -1,6 +1,7 @@
 package com.thock.back.market.in.dto.res;
 
 import com.thock.back.market.domain.CartItem;
+import com.thock.back.market.domain.CartProductView;
 import com.thock.back.market.out.api.dto.ProductInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -59,5 +60,25 @@ public record CartItemResponse (
                 totalSalePrice,
                 totalDiscountAmount
                 );
+    }
+
+    public static CartItemResponse from(CartItem item, CartProductView product) {
+        Long totalPrice = item.getQuantity() * product.getPrice();
+        Long totalSalePrice = item.getQuantity() * product.getSalePrice();
+        Long totalDiscountAmount = totalPrice - totalSalePrice;
+
+        return new CartItemResponse(
+                item.getId(),
+                item.getQuantity(),
+                product.getProductId(),
+                product.getName(),
+                product.getImageUrl(),
+                product.getPrice(),
+                product.getSalePrice(),
+                product.availableStock(),
+                totalPrice,
+                totalSalePrice,
+                totalDiscountAmount
+        );
     }
 }
