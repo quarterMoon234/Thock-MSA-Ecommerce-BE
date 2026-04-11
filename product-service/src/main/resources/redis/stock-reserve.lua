eb5a8b23d18d03c1cf3b20eb4dd4b3700892acc9
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-11 14:54:30 +0900

    task: Lua Script 주석 추가

--- product-service/src/main/resources/redis/stock-reserve.lua
+++ product-service/src/main/resources/redis/stock-reserve.lua
@@ -11,20 +11,23 @@
 -- -1 = stock key missing
 -- -2 = invalid argument
 
+-- 문자열 입력값을 Lua 변수로 맵핑
 local reservationKey = KEYS[#KEYS]
 local ttlSeconds = tonumber(ARGV[1])
 local productCount = tonumber(ARGV[2])
 
+-- 입력값을 검증하는 블록
 if productCount == nil or productCount <= 0 or (#KEYS - 1) ~= productCount then
   return -2
 end
 
+-- 주문 정보가 유효한 지, Redis에 상품이 존재하는 지, Redis 상품과 주문 상품을 뺀 값이 양수인 지 전체적인 검증을 수행하는 블록
 if redis.call('EXISTS', reservationKey) == 1 then
   return 2
 end
 
 for i = 1, productCount do
-  local productIdArgIndex = 3 + ((i - 1) * 2)
+  local productIdArgIndex = 3 + ((i - 1) * 2) -- 3번 인덱스부터 시작 (1, 2번은 메타 정보)
   local quantityArgIndex = productIdArgIndex + 1
   local quantity = tonumber(ARGV[quantityArgIndex])
 
@@ -42,8 +45,9 @@ for i = 1, productCount do
   end
 end
 
+-- 재고 차감
 for i = 1, productCount do
-  local productIdArgIndex = 3 + ((i - 1) * 2)
+  local productIdArgIndex = 3 + ((i - 1) * 2) -- 3번 인덱스부터 시작 (1, 2번은 메타 정보)
   local quantityArgIndex = productIdArgIndex + 1
   local productId = ARGV[productIdArgIndex]
   local quantity = tonumber(ARGV[quantityArgIndex])
