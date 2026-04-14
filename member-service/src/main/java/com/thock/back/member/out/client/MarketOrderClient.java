008e89fc720c835e2c5c953bda236528a049e1de
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-14 15:07:57 +0900

    feat: 결제 서비스 API 호출부 추가

---
+++ member-service/src/main/java/com/thock/back/member/out/client/MarketOrderClient.java
@@ -0,0 +1,24 @@
+package com.thock.back.member.out.client;
+
+import com.thock.back.member.config.MemberFeignConfig;
+import com.thock.back.member.out.client.dto.MarketOrderSummaryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(
+        name = "member-market-order-client",
+        url = "${custom.global.marketServiceUrl}/api/v1/orders/internal",
+        configuration = MemberFeignConfig.class
+)
+public interface MarketOrderClient {
+
+    @GetMapping("/members/{memberId}/")
+    List<MarketOrderSummaryDto> getRecentOrderSummaries(
+            @PathVariable Long memberId,
+            @RequestParam int limit
+    );
+}
